5bbb357610ad8da98d2b8fc9fdc0fb7185530ea8
Jason Costomiris <jcostom@jasons.org>
2023-02-20 22:15:04 -0500

    dependabot updates

--- sbhum.py
+++ sbhum.py
@@ -33,7 +33,7 @@ INFLUX_MEASUREMENT = os.getenv('INFLUX_MEASUREMENT')
 DEBUG = int(os.getenv('DEBUG', 0))
 
 # --- Other Globals ---
-VER = '0.8.1'
+VER = '0.8.3'
 UA_STRING = f"sbhum.py/{VER}"
 URL = 'https://api.switch-bot.com/v1.1/devices/{}/status'
 
